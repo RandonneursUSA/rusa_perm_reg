eb6a5ca1bc7d2d535f2d28ab5dae628a3fd64119
Paul Lieberman <lieb77@gmail.com>
2020-06-26 20:51:57 -0700

    Almost done

--- src/Controller/RusaPermRideReg.php
+++ src/Controller/RusaPermRideReg.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\rusa_perm_reg\Controller;
 
+
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Session\AccountProxy;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,13 +16,16 @@ use Symfony\Component\HttpFoundation\RequestStack;
  */
 class RusaPermRideReg extends ControllerBase {
 
+    protected $currentUser;
     protected $request;
-
+    protected $entityTypeManager;
   /**
    * {@inheritdoc}
    */
-  public function __construct(RequestStack $request) {
-      $this->request = $request;
+  public function __construct(AccountProxy $currentUser, RequestStack $request, EntityTypeManagerInterface $entityTypeManager ) {
+      $this->currentUser       = $currentUser;
+      $this->request           = $request;
+      $this->entityTypeManager = $entityTypeManager;
   } 
 
 	/**
@@ -27,7 +33,9 @@ class RusaPermRideReg extends ControllerBase {
 	 */
 	public static function create(ContainerInterface $container) {
 		return new static(
-          $container->get('request_stack')
+          $container->get('current_user'),
+          $container->get('request_stack'),
+          $container->get('entity_type.manager'),
 		);
 	 }
 
@@ -37,12 +45,23 @@ class RusaPermRideReg extends ControllerBase {
 	 public function resultSubmit() {
 
         // Data was passed in a Post
-        $request  = $this->request->getCurrentRequest();
-        dpm($request->request->all());
+        $request = $this->request->getCurrentRequest();
+        $results = $request->request->all();
+        $regid   = $results['regid'];
+        $rsid    = $results['rsid'];
 
-        $output = ['#markup' => $this->t('All good here')];
+        // Load the registration entity which was passed back to us in $regid
+        $storage = $this->entityTypeManager->getStorage('rusa_perm_reg_ride');
+        $reg     = $storage->load($regid); 
 
-        return $output;
+        // Set the result status id to the rsid that was passed to us
+        $reg->set('field_rsid', $rsid);
+        $reg->set('status', 0);
+        $reg->save();
+
+        // That's it we're done here.
+
+        return $this->redirect('rusa_perm.reg',['user' => $this->currentUser->id()]); 
 	}
 
 
